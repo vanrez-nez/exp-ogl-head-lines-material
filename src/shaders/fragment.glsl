47ccb39ed133737240c82e8bc364e2b95b9406ee
Iván Juárez Núñez <ivan@blindgazer.com>
2021-04-16 13:03:39 -0500

    pending changes

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,11 +6,11 @@ varying vec2 vUv;
 varying vec3 vPos;
 
 void main() {
-  float c = mod(vPos.y + vPos.x * 0.2 + vPos.z * 0.4 + time, 0.02) * 50.;
+  float c = mod(vPos.y + vUv.x + time, 0.02) * 50.;
   float stroke = 0.85 - vPos.z * 0.5;
   float s = smoothstep(stroke, stroke + 0.1, c);
   s = smoothstep(stroke - 0.1, stroke, s);
-  gl_FragColor.rgb = vec3(s);
+  gl_FragColor.rgb = vec3(vUv.xy, 1.0);
   gl_FragColor.a = vPos.z + 0.4;
   if (s < 0.01) discard;
 }
